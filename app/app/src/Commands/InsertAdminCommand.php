dfa51f4b72d644a9f9002fa90199f1e21451e370
mattimatti <mmonti@gmail.com>
2017-03-24 21:26:59 +0100

    insert admin

---
+++ app/app/src/Commands/InsertAdminCommand.php
@@ -0,0 +1,119 @@
+<?php
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class InsertAdminCommand extends AbstractCommand
+{
+
+    protected function configure()
+    {
+        $this->setName('app:insert-admin')
+            ->setDescription('insert Admin')
+            ->setHelp('This command allows you to insert admin in database');
+        
+        $this->addArgument('domain', InputArgument::REQUIRED, 'the domain.');
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \App\Commands\AbstractCommand::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'Load Admin',
+            '============',
+            ''
+        ]);
+        
+        $domain = $input->getArgument('domain');
+        $output->writeln('Domain: ' . $domain);
+        
+        $dbconfig = $this->getDbConfig($domain, $output);
+        
+        $output->writeln(print_r($dbconfig, 1));
+        
+        $out = @system('echo from sys');
+        $output->writeln($out);
+        
+        $user = $dbconfig['user'];
+        $password = $dbconfig['password'];
+        $dbname = $dbconfig['dbname'];
+        
+        $out = @system("mysql -u$user -p$password $dbname < data/fixtures.sql");
+        $output->writeln($out);
+        
+        $output->writeln("Fixtures loaded");
+    }
+
+    /**
+     *
+     * @param unknown $domain            
+     * @param unknown $output            
+     * @throws \Exception
+     * @return multitype:
+     */
+    public function getDbConfig($domain, $output)
+    {
+        $folder = "web/domains/$domain/";
+        
+        if (file_exists($folder)) {
+            $output->writeln("Folder $domain exists");
+        }
+        
+        $settingsfile = $folder . "settings.php";
+        
+        $settings = array();
+        
+        if (file_exists($folder)) {
+            $output->writeln('Settings file exists');
+            $settings = require $settingsfile;
+            $settings = $settings['settings'];
+            
+            if (! is_array($settings)) {
+                throw new \Exception("Cannot load settings file");
+            }
+        } else {
+            $output->writeln('Settings file do not exists');
+            exit();
+        }
+        
+        $config = $this->getSlim()
+            ->getContainer()
+            ->get('settings');
+        
+        $config = $config->all();
+        
+        if (! is_array($config)) {
+            throw new \Exception("Cannot load settings file");
+        }
+        
+        // $output->writeln(print_r($config, 1));
+        // $output->writeln(print_r($settings, 1));
+        
+        $config = array_replace_recursive($config, $settings);
+        
+        if (! is_array($config)) {
+            throw new \Exception("unable to merge");
+        }
+        
+        $dbconfig = $config['database'];
+        
+        $dbsettings = "app/dbsettings.php";
+        
+        if (file_exists($dbsettings)) {
+            $output->writeln('DBSettings file FOUND');
+            $settings = require $dbsettings;
+            $dbconfig = array_replace_recursive($dbconfig, $settings);
+        }
+        
+        return $dbconfig;
+    }
+}
---
+++ app/data/fixtures.sql
@@ -0,0 +1,17 @@
+INSERT INTO `user` (`id`, `username`, `password_hash`, `name`, `surname`)
+VALUES
+	(1, 'admin', '$2y$10$esEDa.lTxt4iwzG1n58/2Ogx74LTimpqIVOpd.DWODnTlI6xH9R6i', NULL, NULL);
+
+	
+	
+INSERT INTO `role` (`id`, `role`)
+VALUES
+	(1, 'admin'),
+	(2, 'hostess');
+
+	
+	
+INSERT INTO `role_user` (`id`, `role_id`, `user_id`)
+VALUES
+	(1, 1, 1),
+	(2, 2, 2);
