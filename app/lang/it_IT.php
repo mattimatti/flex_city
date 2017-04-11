a7ae85710b0677eaec0d5282a987a26e37039eeb
fiorettini <paolo.fiorettini@wauu.it>
2017-04-11 17:14:48 +0200

    Add files via upload

---
+++ app/lang/it_IT.php
@@ -0,0 +1,56 @@
+<?php
+return array(
+    'welcome' => 'Benvenuto',
+
+    'close' => 'Chiudi',
+
+    'subscribe' => 'Invia Registrazione',
+
+    
+    'MALE' => 'Maschio',
+    'FEMALE' => 'Femmina',
+    'YES' => 'Accetto',
+    'NO' => 'Non Accetto',
+    'DAY' => 'Data di nascita: giorno',
+    'MONTH' => 'Data di nascita: mese',
+    'YEAR' => 'Data di nascita: anno',
+    'GENDER' => 'Sesso',
+    
+    'PRODUCT' => 'Prodotto',
+    'SELECT_PRODUCT' => 'Seleziona Prodotto',
+
+    
+    'CSFR_EXCEPTION' => 'Richiesta scaduta',
+    
+    'LEAD_REGISTER_SUCCESS' => 'Grazie per la registrazione',
+    'EMAIL_ALREADY_REGISTERED' => 'Un concorrente con %key% %value% è già registrato',
+    
+    'EMAIL' => 'Indirizzo mail',
+    'EMAIL-placeholder' => 'Inserisci l\'indirizzo di posta elettronica',
+
+    'NAME' => 'Nome',
+    'NAME-placeholder' => 'Inserisci il nome',
+    
+    'SURNAME' => 'Cognome',
+    'SURNAME-placeholder' => 'Inserisci il cognome',
+
+    'CITY' => 'Città',
+    'CITY-placeholder' => 'Inserisci la città',
+    
+    'PHONE' => 'Telefono',
+    'PHONE-placeholder' => 'Inserisci il telefono',
+    
+    'privacy-modal-title' => 'Informativa sul trattamento dei dati personali',
+    'terms-modal-title' => 'Regolamento del concorso',
+    'marketing-modal-title' => 'Consenso all’invio di materiale promo-pubblicitario',
+    'marketing-third-modal-title' => 'Consenso  alla comunicazione dei dati alle società facenti parte del gruppo VF International',
+    
+    'privacy-label' => 'Dichiaro di aver preso visione dell’<a target="_blank" href="/media/pdf/privacy-policy.pdf">informativa sul trattamento dei dati</a> e di accettare <a target="_blank" href="/media/pdf/terms-and-conditions.pdf">il regolamento</a> del concorso*.',
+    'terms-label' => 'Dichiaro di accettare il regolamento del concorso.',
+    'marketing-label' => 'Acconsento all’invio di materiale promo-pubblicitario tramite posta elettronica da parte di VF Italia srl.',
+    'marketing-third-label' => 'Acconsento alla comunicazione dei miei dati alle società del gruppo VF International, quali VF Italia, le  legal entities delle altre country e i partner franchisee, per finalità promozionali e di marketing delle stesse.',
+    
+    
+    
+    'random' => 'Sono un testo ad minchiam, mi usano come esempio, scusatemi.'
+);
