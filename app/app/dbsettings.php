a75bfeea86ca9184aeedd13d7c5c88024498fabd
mattimatti <mmonti@gmail.com>
2017-04-12 14:22:53 +0200

    added dbsettings

---
+++ app/app/dbsettings.php
@@ -0,0 +1,15 @@
+<?php
+
+//
+//
+if (ENVIRONMENT == 'development') {
+    return [
+        'user' => 'root',
+        'password' => ''
+    ];
+}
+
+return [
+    'user' => 'cb26zfki_flexapp',
+    'password' => 'nvNBM@{4qU*i'
+];
