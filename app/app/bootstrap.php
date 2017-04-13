3abeee182a1d865f495bab70e158eaa76de9db4b
mattimatti <mmonti@gmail.com>
2017-04-13 10:24:58 +0200

    ok bootstrap

--- app/app/bootstrap.php
+++ app/app/bootstrap.php
@@ -36,23 +36,21 @@ if (defined('HOST_DOMAIN')) {
     }
 }
 
-
-
-$filename = __DIR__ . '/../web/domains/' . CURRENT_DOMAIN . '/settings.php';
-if (file_exists($filename)) {
-    $country_settings = require $filename;
-    
-    if (ENVIRONMENT == 'development') {
-        unset($country_settings['settings']['database']);
+if (defined('CURRENT_DOMAIN')) {
+    $filename = __DIR__ . '/../web/domains/' . CURRENT_DOMAIN . '/settings.php';
+    if (file_exists($filename)) {
+        $country_settings = require $filename;
+        
+        if (ENVIRONMENT == 'development') {
+            unset($country_settings['settings']['database']);
+        }
+        
+        if (is_array($country_settings)) {
+            $settings = array_replace_recursive($settings, $country_settings);
+        }
+    } else {
+        exit("error in db config: " . CURRENT_DOMAIN . " - ");
     }
-    
-    
-    if (is_array($country_settings)) {
-        $settings = array_replace_recursive($settings, $country_settings);
-    }
-    
-} else {
-    exit("error in db config: " . CURRENT_DOMAIN . " - ");
 }
 
 $app = new \Slim\App($settings);
