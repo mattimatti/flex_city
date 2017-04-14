2761f0da59f5b7882b5817ea87fc6f550151f093
fiorettini <paolo.fiorettini@wauu.it>
2017-04-14 15:40:59 +0200

    Update nl_NL.php

--- app/lang/nl_NL.php
+++ app/lang/nl_NL.php
@@ -2,51 +2,51 @@
 return array(
     'welcome' => 'Welkom',
 
-    'close' => 'Chiudi',
+    'close' => 'Sluit',
 
-    'subscribe' => 'Invia Registrazione',
+    'subscribe' => 'Verstuur registratie',
 
     
     'MALE' => 'Man',
     'FEMALE' => 'Vrouw',
-    'YES' => 'Accetto',
-    'NO' => 'Non Accetto',
+    'YES' => 'Ik accepteer',
+    'NO' => 'Ik accepteer niet',
     'DAY' => 'Geboortedatum: dag',
     'MONTH' => 'Geboortedatum: maand',
     'YEAR' => 'Geboortedatum: jaar',
-    'GENDER' => 'Sesso',
+    'GENDER' => 'Geslacht',
     
-    'PRODUCT' => 'Prodotto',
-    'SELECT_PRODUCT' => 'Seleziona Prodotto',
+    'PRODUCT' => 'Product',
+    'SELECT_PRODUCT' => 'Selecteer product',
 
     
-    'CSFR_EXCEPTION' => 'Richiesta scaduta',
+    'CSFR_EXCEPTION' => 'Verzoek verlopen',
     
-    'LEAD_REGISTER_SUCCESS' => 'Grazie per la registrazione',
-    'EMAIL_ALREADY_REGISTERED' => 'Un concorrente con %key% %value% è già registrato',
+    'LEAD_REGISTER_SUCCESS' => 'Bedankt voor je registratie',
+    'EMAIL_ALREADY_REGISTERED' => 'Dit e-mail adres is al eens geregistreerd.',
     
-    'EMAIL' => 'Indirizzo mail',
-    'EMAIL-placeholder' => 'Inserisci l\'indirizzo di posta elettronica',
+    'EMAIL' => 'E-mail adres',
+    'EMAIL-placeholder' => 'Voer e-mail adres in',
 
-    'NAME' => 'Voornaam',
-    'NAME-placeholder' => 'Inserisci il nome',
+    'NAME' => 'Naam',
+    'NAME-placeholder' => 'Naam',
     
     'SURNAME' => 'Achternaam',
-    'SURNAME-placeholder' => 'Inserisci il cognome',
+    'SURNAME-placeholder' => 'Achternaam',
 
-    'CITY' => 'Stad',
-    'CITY-placeholder' => 'Inserisci la città',
+    'CITY' => 'Plaats',
+    'CITY-placeholder' => 'Plaats',
     
     'PHONE' => 'Telefoon',
-    'PHONE-placeholder' => 'Inserisci il telefono',
+    'PHONE-placeholder' => 'Telefoonnummer',
     
-    'privacy-modal-title' => 'Informativa sul trattamento dei dati personali',
-    'terms-modal-title' => 'Regolamento del concorso',
-    'marketing-modal-title' => 'Consenso all’invio di materiale promo-pubblicitario',
-    'marketing-third-modal-title' => 'Consenso  alla comunicazione dei dati alle società facenti parte del gruppo VF International',
+    'privacy-modal-title' => 'Informatieve berichtgeving voor de verwerking van persoonsgegevens.',
+    'terms-modal-title' => 'Regelgeving van de wedstrijd',
+    'marketing-modal-title' => 'Geef toestemming voor het ontvangen van promotie en advertentie materiaal.',
+    'marketing-third-modal-title' => 'Geef toestemming voor het communiceren van gegevens met VF International Group partners',
     
     'privacy-label' => 'Ik verklaar dat ik de <a target="_blank" href="/media/pdf/privacy-policy.pdf">informatieve mededeling</a> inzake gegevensverwerking heb gelezen en accepteer <a target="_blank" href="/media/pdf/terms-and-conditions.pdf">de regelgeving</a> van de competitie*.',
-    'terms-label' => 'Dichiaro di accettare il regolamento del concorso.',
+    'terms-label' => 'Ik geef aan dat ik de regelgeving van de wedstrijd accepteer.',
     'marketing-label' => 'Ik accepteer dat VF Italia Srl en VF Italy Retail Srl gebruik maken van mijn persoonlijke informatie voor marketing, promotie, marktonderzoek en profilering doeleinden.',
     'marketing-third-label' => 'Ik accepteer dat mijn informatie kan worden gedeeld met partners van de VF International groep, zoals VF Italia, rechtspersonen uit andere landen en partner franchisee ondernemingen voor promotie- en marketingdoeleinde.',
     
