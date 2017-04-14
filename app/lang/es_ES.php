786982a3a83878305f9298166464987a09b894bc
fiorettini <paolo.fiorettini@wauu.it>
2017-04-14 15:22:31 +0200

    Update es_ES.php

--- app/lang/es_ES.php
+++ app/lang/es_ES.php
@@ -2,51 +2,51 @@
 return array(
     'welcome' => 'Bienvenido',
 
-    'close' => 'Chiudi',
+    'close' => 'Cierra',
 
-    'subscribe' => 'Invia Registrazione',
+    'subscribe' => 'Envía inscripción',
 
     
     'MALE' => 'Hombre',
     'FEMALE' => 'Mujer',
-    'YES' => 'Accetto',
-    'NO' => 'Non Accetto',
+    'YES' => 'Acepto',
+    'NO' => 'No acepto',
     'DAY' => 'Fecha de nacimiento: día',
     'MONTH' => 'Fecha de nacimiento: mes',
     'YEAR' => 'Fecha de nacimiento: año',
-    'GENDER' => 'Sesso',
+    'GENDER' => 'Sexo',
     
-    'PRODUCT' => 'Prodotto',
-    'SELECT_PRODUCT' => 'Seleziona Prodotto',
+    'PRODUCT' => 'Producto',
+    'SELECT_PRODUCT' => 'Selecciona producto',
 
     
-    'CSFR_EXCEPTION' => 'Richiesta scaduta',
+    'CSFR_EXCEPTION' => 'Solicitud vencida',
     
-    'LEAD_REGISTER_SUCCESS' => 'Grazie per la registrazione',
-    'EMAIL_ALREADY_REGISTERED' => 'Un concorrente con %key% %value% è già registrato',
+    'LEAD_REGISTER_SUCCESS' => 'Gracias por la inscripción',
+    'EMAIL_ALREADY_REGISTERED' => 'Un participante ya está inscrito con este e-mail',
     
-    'EMAIL' => 'Indirizzo mail',
-    'EMAIL-placeholder' => 'Inserisci l\'indirizzo di posta elettronica',
+    'EMAIL' => 'Dirección e-mail',
+    'EMAIL-placeholder' => 'Introduce la dirección de correo electrónico',
 
     'NAME' => 'Nombre',
-    'NAME-placeholder' => 'Inserisci il nome',
+    'NAME-placeholder' => 'Introduce el nombre',
     
     'SURNAME' => 'Apellidos',
-    'SURNAME-placeholder' => 'Inserisci il cognome',
+    'SURNAME-placeholder' => 'Introduce los apellidos',
 
     'CITY' => 'Ciudad',
-    'CITY-placeholder' => 'Inserisci la città',
+    'CITY-placeholder' => 'Introduce la ciudad',
     
     'PHONE' => 'Teléfono',
-    'PHONE-placeholder' => 'Inserisci il telefono',
+    'PHONE-placeholder' => 'Introduce el teléfono',
     
-    'privacy-modal-title' => 'Informativa sul trattamento dei dati personali',
-    'terms-modal-title' => 'Regolamento del concorso',
-    'marketing-modal-title' => 'Consenso all’invio di materiale promo-pubblicitario',
-    'marketing-third-modal-title' => 'Consenso  alla comunicazione dei dati alle società facenti parte del gruppo VF International',
+    'privacy-modal-title' => 'Nota Informativa sobre el tratamiento de los datos personales',
+    'terms-modal-title' => 'Reglamento del concurso',
+    'marketing-modal-title' => 'Autorización para el envío de material promo-publicitario',
+    'marketing-third-modal-title' => 'Autorización para la comunicación de los datos a las sociedades que forman parte del grupo VF International',
     
     'privacy-label' => 'Declaro que he leído la <a target="_blank" href="/media/pdf/privacy-policy.pdf">nota informativa sobre el tratamiento de los datos</a> de carácter personal y que acepto <a target="_blank" href="/media/pdf/terms-and-conditions.pdf">el reglamento</a> del concurso*.',
-    'terms-label' => 'Dichiaro di accettare il regolamento del concorso.',
+    'terms-label' => 'Declaro que acepto el reglamento del concurso.',
     'marketing-label' => 'Autorizo el tratamiento de mis datos personales por parte de VF Italia Srl y VF Italy Retail srl, para fines de marketing, promocionales, estudios de mercado y elaboración de perfiles.',
     'marketing-third-label' => 'Autorizo la comunicación de mis datos personales a las sociedades del grupo VF International, como VF Italia, las entidades jurídicas en otros países y a sus franquiciados, para fines promocionales y de marketing de las mismas.',
     
