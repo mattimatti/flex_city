42f38363b1517afdf8399d733e290cf1ce5b8112
fiorettini <paolo.fiorettini@wauu.it>
2017-04-14 17:35:56 +0200

    Add files via upload

--- app/web/domains/flexinthecity.timberland.at/settings.php
+++ app/web/domains/flexinthecity.timberland.at/settings.php
@@ -2,12 +2,12 @@
 return [
     'settings' => [
         'lang' => 'de_DE',
-        'domain' => 'flexinthecity.timberland.at',
+        'domain' => 'flexinthecity.timberland.de',
         'database' => [
             'host' => 'localhost',
             'user' => 'cb26zfki_web',
             'password' => '6c3bSizR?a[_',
-            'dbname' => 'cb26zfki_simfony_at'
+            'dbname' => 'cb26zfki_simfony_de'
         ],
     ]
 ];
