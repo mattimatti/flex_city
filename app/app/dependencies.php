1a1328a2c9697de4bcb73f61bc5034302b730a7b
mattimatti <mmonti@gmail.com>
2017-04-18 12:07:04 +0200

    All

--- app/app/dependencies.php
+++ app/app/dependencies.php
@@ -238,25 +238,25 @@ $container['mailService'] = function ($c)
     
     
     
-    $mailer->SMTPDebug = 2; // enables SMTP debug information (for testing)
+//     $mailer->SMTPDebug = 2; // enables SMTP debug information (for testing)
     
-    $mailer->SMTPAuth = true; // enable SMTP authentication
-//     $mailer->SMTPSecure = "tls"; // sets the prefix to the servier
-//     $mailer->SMTPSecure = "ssl"; // sets the prefix to the servier
-    $mailer->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-//     $mailer->Port = 587; // set the SMTP port for the GMAIL server
-//     $mailer->Port = 465; // set the SMTP port for the GMAIL server
-    $mailer->Port = 25; // set the SMTP port for the GMAIL server
-    $mailer->Username = "noreply.flexinthecity@gmail.com"; // GMAIL username
-    $mailer->Password = "Flex7102"; // GMAIL password
-    $mailer->isSMTP();
+//     $mailer->SMTPAuth = true; // enable SMTP authentication
+// //     $mailer->SMTPSecure = "tls"; // sets the prefix to the servier
+// //     $mailer->SMTPSecure = "ssl"; // sets the prefix to the servier
+//     $mailer->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+// //     $mailer->Port = 587; // set the SMTP port for the GMAIL server
+// //     $mailer->Port = 465; // set the SMTP port for the GMAIL server
+//     $mailer->Port = 25; // set the SMTP port for the GMAIL server
+//     $mailer->Username = "noreply.flexinthecity@gmail.com"; // GMAIL username
+//     $mailer->Password = "Flex7102"; // GMAIL password
+//     $mailer->isSMTP();
     
     
     
     
     
     // use sendmail?? NO IF GMAIL
-    // $mailer->IsSendmail();
+    $mailer->IsSendmail();
     
     $renderer = $c->get('mailRenderer');
     $service = new MailService($mailer, $renderer);
