5ebf2dfa1cce00fed35af0af5a1c740094a28aef
mattimatti <mmonti@gmail.com>
2017-04-18 12:49:01 +0200

    All

--- app/web/dist/js/main.js
+++ app/web/dist/js/main.js
@@ -16880,4 +16880,17 @@ $( document ).ready(function() {
 	$(".autosubmit").change(function() {
 	    $(this).closest('form').submit();
 	});
+	
+	
+	$('a[data-confirm]').click(function(ev) {
+		var href = $(this).attr('href');
+		var title = $(this).attr('data-confirm');
+		if(confirm(title)){
+			 window.location.href = href;
+			 return false;
+		}
+		return false;
+	});
+	
+	
 });
