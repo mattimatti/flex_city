d82f9ebbe60aa6a625b49ddf40fa48d6b7046e1c
mattimatti <mmonti@gmail.com>
2017-04-18 12:48:11 +0200

    All

--- app/web/src/js/main.js
+++ app/web/src/js/main.js
@@ -3,4 +3,17 @@ $( document ).ready(function() {
 	$(".autosubmit").change(function() {
 	    $(this).closest('form').submit();
 	});
+	
+	
+	$('a[data-confirm]').click(function(ev) {
+		var href = $(this).attr('href');
+		var title = $(this).attr('data-confirm');
+		if(confirm(title)){
+			 window.location.href = href;
+			 return false;
+		}
+		return false;
+	});
+	
+	
 });
